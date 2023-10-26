3ea9550509edb49ae22d0d64c86d1efae3577048
Curtis <Curtis.David7@gmail.com>
2023-10-26 16:02:45 -0400

    create regression pipeline and disable long winded confirmations tests

--- e2e/specs/confirmations/eth-sign.spec.js
+++ e2e/specs/confirmations/eth-sign.spec.js
@@ -9,12 +9,11 @@ import {
   withFixtures,
   defaultGanacheOptions,
 } from '../../fixtures/fixture-helper';
-import { Regression } from '../../tags';
 import TestHelpers from '../../helpers';
 
 const MAX_ATTEMPTS = 3;
 
-describe(Regression('Eth Sign'), () => {
+describe('Eth Sign', () => {
   beforeAll(async () => {
     jest.setTimeout(2500000);
     await TestHelpers.reverseServerPort();
